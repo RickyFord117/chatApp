95cd53eff9d18bc41c4e2b5c5ed4f4cbd491e144
Rick <ricardo.henriques@accedo.tv>
2024-02-29 13:54:46 +0000

    initial dev state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,8 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-
-import { useColorScheme } from "@/components/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -44,11 +37,9 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
     <Stack>
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen name='index' options={{ headerShown: false }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const index = () => {
+const Page = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>FIRST PAGE</Text>
     </View>
   );
 };
 
-export default index;
+export default Page;
