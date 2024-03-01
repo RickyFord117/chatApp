d1d9076ba60d23313020ec2152349693ee760345
Rick <ricardo.henriques@accedo.tv>
2024-03-01 15:48:45 +0000

    some routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,32 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Link } from "expo-router";
+
 import welcomeImage from "@/assets/images/welcome.png";
+import Colors from "@/constants/Colors";
+
 const welcome_image = Image.resolveAssetSource(welcomeImage).uri;
 
 const Page = () => {
+  const openLink = () => {};
   return (
     <View style={styles.container}>
       <Image source={{ uri: welcome_image }} style={styles.welcome} />
-      <Text>FIRST PAGE</Text>
+      <Text style={styles.headline}>Welcome to ChatApp</Text>
+      <Text style={styles.description}>
+        Read our{" "}
+        <Text style={styles.link} onPress={openLink}>
+          Privacy Policy
+        </Text>
+        . {'Tap "Agree & Continue" to accept the '}
+        <Text style={styles.link} onPress={openLink}>
+          Terms of Service
+        </Text>
+      </Text>
+      <Link href={"/otp"} asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Agree & Continue</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 };
@@ -17,11 +37,35 @@ const styles = StyleSheet.create({
     padding: 20,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
+    backgroundColor: "#fff",
   },
   welcome: {
     width: "100%",
     height: 300,
+    marginBottom: 80,
+  },
+  headline: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 80,
+    color: Colors.gray,
+  },
+  link: {
+    color: Colors.primary,
+  },
+  button: {
+    width: "100%",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: Colors.primary,
   },
 });
 
